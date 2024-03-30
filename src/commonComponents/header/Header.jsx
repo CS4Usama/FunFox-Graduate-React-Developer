891b957a452a06c54f749dee2e5b20da71e718cb
Cyber Hawk <cs.cybernetic@gmail.com>
2024-03-30 15:03:29 +0500

    slide-1 responsiveness

--- src/commonComponents/header/Header.jsx
+++ src/commonComponents/header/Header.jsx
@@ -5,8 +5,13 @@ import FunFoxLogo from "../../assets/funfox-logo.svg";
 export default function Header(props) {
     return (
         <>
-            <img src={FunFoxLogo} alt="FunFox Logo" draggable='false' />
-            <img src={props.img} className='week-1' alt="Week 1" draggable='false' />
+            <img src={FunFoxLogo} className='funfox-logo' alt="FunFox Logo" draggable='false' />
+            {
+                props.isImage ?
+                    null
+                :
+                    <img src={props.img} className='week-1' alt="Week 1" draggable='false' />
+            }
         </>
     );
 }
--- src/modules/slide-2/Slide2.jsx
+++ src/modules/slide-2/Slide2.jsx
@@ -1,10 +1,12 @@
 import "./Slide2.css";
 import "../../App.css";
+import Header from "../../commonComponents/header/Header";
 
 
 export default function Slide2() {
     return (
         <>
+            <Header isImage='false' />
             This is Slide 2 Page.
         </>
     );
